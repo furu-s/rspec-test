584ca7c359f71a66df38cdb52b18ffb4c00c0f42
furu-s <furu.s11@gmail.com>
2016-09-24 15:25:53 +0900

    coverall settings

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 [![Build Status](https://travis-ci.org/furu-s/rspec-test.svg?branch=master)](https://travis-ci.org/furu-s/rspec-test)
 [![Code Climate](https://codeclimate.com/github/furu-s/rspec-test/badges/gpa.svg)](https://codeclimate.com/github/furu-s/rspec-test)
+[![Test Coverage](https://codeclimate.com/github/furu-s/rspec-test/badges/coverage.svg)](https://codeclimate.com/github/furu-s/rspec-test/coverage)
 
 ## Thanks
 http://qiita.com/dtan4/items/13b0ea9edf5b99926446
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,14 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+if ENV['CI']
+  require 'coveralls'
+  Coveralls.wear!
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[Coveralls::SimpleCov::Formatter]
+  SimpleCov.start 'test_frameworks'
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
